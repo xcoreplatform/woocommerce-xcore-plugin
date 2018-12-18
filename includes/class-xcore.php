c71fc9143a416f2884ec2935b7b905bba74d38a7
Willie Huizing <w.huizing@linq5.nl>
2018-12-18 16:02:24 +0100

    version correction

--- includes/class-xcore.php
+++ includes/class-xcore.php
@@ -4,7 +4,7 @@ defined('ABSPATH') || exit;
 
 class Xcore
 {
-    private          $_version         = '1.4.1';
+    private          $_version         = '1.4.2';
     protected static $_instance        = null;
     protected static $_productInstance = null;
 
