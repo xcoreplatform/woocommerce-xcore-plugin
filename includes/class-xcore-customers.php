d777cafb765241da392b532e6ecd1643e1a22e0b
Henri Nijborg <h.nijborg@linq5.nl>
2019-10-09 12:18:25 +0200

    Handle timezone

--- includes/class-xcore-customers.php
+++ includes/class-xcore-customers.php
@@ -84,7 +84,8 @@ class Xcore_Customers extends WC_REST_Customers_Controller
 
         $limit = (int)$request['limit'] ?: 50;
 
-        $key   = 'date_modified';
+        $timezoneOffset = wc_timezone_offset();
+
         $value = $request['date_modified'] ?: 0;
         $value = str_ireplace('T', ' ', $value);
 
@@ -94,7 +95,7 @@ class Xcore_Customers extends WC_REST_Customers_Controller
         $q = "
             SELECT ID as id, user_registered as date_created, 
                 CASE 
-                    WHEN meta_value IS NOT NULL THEN FROM_UNIXTIME(meta_value)
+                    WHEN meta_value IS NOT NULL THEN DATE_ADD(FROM_UNIXTIME(meta_value), INTERVAL %s SECOND)
                     ELSE user_registered 
                 END AS date_modified
             FROM {$wp_users_table} AS users 
@@ -107,7 +108,7 @@ class Xcore_Customers extends WC_REST_Customers_Controller
             ORDER BY date_modified ASC LIMIT %d
 		";
 
-        $sql     = $wpdb->prepare($q, array($key, $value, $value, $limit));
+        $sql     = $wpdb->prepare($q, array($timezoneOffset, $value, $limit));
         $results = $wpdb->get_results($sql, ARRAY_A);
 
         foreach ($results as $key => $value) {
