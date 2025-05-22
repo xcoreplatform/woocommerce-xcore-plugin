6650560f6086c931589787f4764ed8ffee503318
Willie Huizing <w.huizing@linq5.nl>
2025-05-22 08:48:38 +0200

    1.14.5

--- includes/class-xcore.php
+++ includes/class-xcore.php
@@ -3,7 +3,7 @@
 defined( 'ABSPATH' ) || exit;
 
 class Xcore {
-	private $_version            = '1.14.4';
+	private $_version            = '1.14.5';
 	private static $_instance    = null;
     private        $_xcoreHelper = null;
 
